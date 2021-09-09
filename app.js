1baad0b82152a9edabaf301fdfd7abe386a7951d
Zachary Hall <zacharyhall495@gmail.com>
2021-09-09 13:57:46 -0500

    java commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let friends = ["Dan", "Jason", "Blake", "Will", "Ty"];
+let songCount = 99;
+
+function singingFriends(friends) {
+  let name = "";
+  let y = 0;
+
+  for (let i = 0; i < friends.length; i++) {
+    name = friends[i];
+
+    for (let x = songCount; x > 1; x--)
+      if (x > 1) {
+        y = x - 1;
+
+        console.log(
+          x +
+            "lines of code to write" +
+            x +
+            "lines of code;" +
+            name +
+            "strikes one out, leaves a comment" +
+            y +
+            "codes to write"
+        );
+      } else {
+        console.log(
+          x +
+            "line of code to write" +
+            x +
+            "more line of code;" +
+            name +
+            "strikes it out, leaves a comment. No more lines of code left to write"
+        );
+      }
+  }
+}
+
+singingFriends(friends);
